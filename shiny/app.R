476a72c8be2f56250b29ff12d9e71727ea777489
Lorraine Jiao <lorrainejiao@outlook.com>
2021-12-10 15:00:22 -0500

    Adding us map onto shiny.io

--- shiny/app.R
+++ shiny/app.R
@@ -21,7 +21,30 @@ new = gun %>%
   group_by(state,year) %>% 
   summarize(number = n()) 
 
+
+gun_state = gun %>%
+  group_by(year, state) %>% 
+  summarise(number_of_incident = n(), 
+            sum_killed = sum(number_killed), 
+            sum_injured = sum(number_injured)) %>% 
+  replace(is.na(.), "District of Columbia") 
+
+url <- 'https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_20m.json'
+states <- fromJSON(file=url)
+
+
+
+
 ui = navbarPage("My Application",
+    tabPanel("US map for mass shooting",
+                         sidebarPanel(
+                           sliderInput(
+                             "year_select", 
+                             "Select year",
+                             min = 2018, max = 2021, value = 2020)
+                         ),
+                         mainPanel(plotlyOutput("usplot"))), 
+    
     tabPanel("unemployment rate",
                sidebarPanel(
                  selectInput(
@@ -29,17 +52,7 @@ ui = navbarPage("My Application",
                    "Select state",
                    choices = state, selected = "Alabama")
                ),
-             mainPanel(plotOutput("lineplot"))
-                                   ),
-    tabPanel("US map for mass shooting",
-                sidebarPanel(
-                  sliderInput(
-                    "year_select", 
-                    "Select year",
-                    min = 2018, max = 2021, value = 2020)
-                ),
-             mainPanel(plotOutput("usplot"))),
-                          tabPanel("Component 3")
+             mainPanel(plotOutput("lineplot")))
 )
 
 
@@ -58,16 +71,34 @@ server = function(input,output){
     )
   })
     
-    output$usplot = renderPlot({
-      new %>% 
-        filter(year == input$year_select) %>% 
-        plot_usmap(data = ., values = "number") +
-        labs(fill = 'number of mass shooting') + 
-        scale_fill_gradientn(colours=rev(heat.colors(20)), na.value = 0,guide = guide_colourbar(barwidth = 0.5, barheight = 10)) +
-        theme(legend.position = "right")
-      
-                                                     
-    })
+    output$usplot = renderPlotly({
+    gun_state %>% 
+      filter(year == input$year_select) %>% 
+      mutate(text_label = str_c("Number of killed: ", sum_killed, '\nNumber of injured: ', sum_injured)) %>%
+      plot_ly() %>% 
+      add_trace(
+        type = "choroplethmapbox",
+        featureidkey = "properties.NAME", 
+        geojson = states,
+        locations= ~state,
+        z=~number_of_incident,
+        colorscale="Viridis", 
+        # add text
+        text = ~text_label,
+        marker = list(line = list(
+          width = 0),
+          opacity = 0.75
+        )
+      ) %>% 
+      layout(
+        mapbox = list(
+          style="carto-positron",
+          zoom = 2,
+          center = list(lon = -95.71, lat = 37.09)), 
+        title = "US Map of Mass Shooting",
+        legend = list(x = 100, y = 0.5)) %>%   
+      colorbar(title = "Mass Shooting")
+  })
 }
 
 shinyApp(ui, server)
